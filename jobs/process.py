6a44a08f0a5526053359fb83ad7d66701be02dc9
Lukasz Mielczarek <mielczarek.lukasz.karol@gmail.com>
2026-07-01 23:38:36 +0200

    Update process.py to modify data source paths and add debugging prints; add output CSV files for demographic insights

--- jobs/process.py
+++ jobs/process.py
@@ -1,15 +1,21 @@
+import os
 from pyspark.sql import SparkSession
 from pyspark.sql.functions import col, sum, avg
+
+print(os.getcwd())
+print("SCRIPT DIR:", os.path.dirname(os.path.abspath(__file__)))
+print("CWD:", os.getcwd())
 
 # Create a Spark session
 spark = SparkSession.builder \
     .appName("Demographic Insights") \
+    .master("spark://master:7077") \
     .getOrCreate()
 
-basepath = "/opt/bitnami/spark/jobs"
+basepath = "/opt/spark"
 
 # Read the CSV file into a DataFrame
-df = spark.read.csv(basepath + "/demographic_divident.csv", header=True, inferSchema=True)
+df = spark.read.csv(basepath + "/data/data.csv", header=True, inferSchema=True)
 
 # Average Life Expectancy by Location
 avg_life_expectancy = df.groupBy("Location") \
